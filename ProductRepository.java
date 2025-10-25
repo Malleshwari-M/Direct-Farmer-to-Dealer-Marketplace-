f9ab58243256ca39ca2cbcdffca052950c00cc04
Malleshwari M <malleshwarimayilsamy@gmail.com>
2025-10-25 17:13:44 +0530

    Create ProductRepository interface for Product entity

---
+++ ProductRepository.java
@@ -0,0 +1,6 @@
+package com.farmerdealer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.farmerdealer.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {}
