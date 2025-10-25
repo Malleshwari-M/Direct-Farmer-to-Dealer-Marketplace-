98832aeafb2868d01ad588d6dc15dc28bdcfbf7f
Malleshwari M <malleshwarimayilsamy@gmail.com>
2025-10-25 17:14:06 +0530

    Initialize Spring Boot application structure

---
+++ FarmerDealerApplication.java
@@ -0,0 +1,11 @@
+package com.farmerdealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerDealerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FarmerDealerApplication.class, args);
+  }
+}
