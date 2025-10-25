473f18b64ccde1febe09584162dcff9cf752a1da
Malleshwari M <malleshwarimayilsamy@gmail.com>
2025-10-25 17:21:40 +0530

    Update FarmerController.java

--- FarmerController.java
+++ FarmerController.java
@@ -4,24 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.farmerdealer.model.Product;
 import com.farmerdealer.service.ProductService;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/farmer")
+@RequestMapping("/products")
 @CrossOrigin(origins = "*")
-public class FarmerController {
+public class ProductController {
 
-  @Autowired
-  private ProductService productService;
+    @Autowired
+    private ProductService productService;
 
-  @PostMapping("/addProduct")
-  public Product addProduct(@RequestBody Product product) {
-    return productService.saveProduct(product);
-  }
-
-  @GetMapping("/products")
-  public List<Product> getProducts() {
-    return productService.getAllProducts();
-  }
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
